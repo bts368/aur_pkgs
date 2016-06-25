e8f5d6d0d95833e4bcb9af6225eebde0a000fe8d
brent s <r00t@square-r00t.net>
2016-06-24 20:32:39 -0400

    about to test the package fetching..

--- _bin/submodule.update.sh
+++ _bin/submodule.update.sh
@@ -34,19 +34,39 @@ function freshenrepo () {
 	REPO="${1}"
 
 	# Check to see if it exists. https://stackoverflow.com/questions/12641469/list-submodules-in-a-git-repository
-	git config --file .gitmodules --get-regexp path | awk '{ print $2 }' | egrep -Eq "^${REPO}"
+	# We COULD use 'if git submodule status "${REPO}"' but we don't because regexes are cooler. so there. deal with it. /me puts on cool guy sunglasses.
+	git config --file .gitmodules --get-regexp path | awk '{ print $2 }' | egrep -Eq "^${REPO}$"
 	if [[ "${?}" -eq '0' ]];
 	then
 		# We remove it so we can grab a fresh copy directly from the AUR.
-		#https://stackoverflow.com/questions/1260748/how-do-i-remove-a-submodule
-		git rm --cached ${REPO}
-		git submodule deinit ${REPO}
-		
-	
+		#https://stackoverflow.com/questions/1260748/how-do-i-remove-a-submodule and others
+		git submodule deinit -f "${REPO}"
+		git rm -f "${REPO}"
+		git config -f .gitmodules --remove-section "submodule.${REPO}" > /dev/null 2>&1  # probably not necessary,...
+		rm -rf .git/modules/${REPO}
+	fi
+	touch .gitmodules
+	git add .gitmodules
+	git submodule add ${URI}:${REPO}
+	cat >> .git/modules/${REPO}/hooks/pre-commit << EOF
+#!/bin/bash
+/usr/bin/mksrcinfo
+
+git add .SRCINFO
+EOF
+	chmod 700 .git/modules/${REPO}/hooks/pre-commit
+
 }
 
-if 
-for i in $(ssh aur@aur.archlinux.org list-repos | sed -e 's/[[:space:]]*//g' | sort);
-do
-	freshenrepo ${i}	
-done
+if [[ "${PKGNAME}" =~ [Aa][Ll]{2} ]];
+then
+	for i in $(ssh aur@aur.archlinux.org list-repos | sed -e 's/[[:space:]]*//g' | sort);
+	do
+		freshenrepo ${i}
+	done
+elif [ -n "${PKGNAME}" ];
+then
+	freshenrepo ${PKGNAME}
+fi
+
+git submodule update
